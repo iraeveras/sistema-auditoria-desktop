d8f7620fee5e7dd6c3ebe40546a05fd128c549f1
Irae Veras <irae.online@gmail.com>
2025-03-20 19:51:49 -0300

    feat: import da page de usuário

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,18 +1,20 @@
 // File: src/ui/App.tsx
 import React from "react";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import UserPage from "./pages/UserPage";
 import Category from "./pages/CategoryPage";
 const App: React.FC = () => {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path="/cadastros/categoria" element={<Category/>}/>
-        <Route path="*" element={<Login/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cadastros/usuario" element={<UserPage />} />
+        <Route path="/cadastros/categoria" element={<Category />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
