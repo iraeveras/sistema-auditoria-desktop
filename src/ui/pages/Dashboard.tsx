87941e8f8c8eb3ad432f3b0f9f72e402cfea5b18
Irae Veras <irae.online@gmail.com>
2025-03-21 16:39:16 -0300

    feat: imports dos Charts e Card

--- src/ui/pages/Dashboard.tsx
+++ src/ui/pages/Dashboard.tsx
@@ -1,5 +1,9 @@
 // File: src/ui/pages/Dashboard.tsx
 import React, { useEffect, useState } from "react";
+import SalesChartCard from "../components/dashboard/charts/SalesChartCard";
+import SalesLineChartCard from "../components/dashboard/charts/SalesLineChartCard";
+import TotalSalesCard from "../components/dashboard/cards/TotalSalesCard";
+import SalesByStoreChart from "../components/dashboard/charts/SalesByStoreChart";
 
 const Dashboard: React.FC = () => {
     const [userName, setUserName] = useState('');
@@ -14,12 +18,64 @@ const Dashboard: React.FC = () => {
     }, [])
 
     return (
-        <div className="flex flex-col min-h-screen">
-            <header className="py-3 p-4 bg-gray-800 text-white">Dashboard</header>
-            <main className="flex-1 p-4">
+        <div className="flex flex-col min-h-screen bg-neutral-50 overflow-hidden">
+            <header className="fixed top-0 w-full z-10 py-3 p-4 bg-neutral-800 text-white">Dashboard</header>
+            <main className="flex-1 mt-11 p-4 overflow-y-scroll mb-11">
+                <div className="flex flex-col gap-4">
+                    <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+                        <div className="sm:cols-4 col-span-1">
+                            <TotalSalesCard />
+                        </div>
+                        <div className="sm:cols-4 col-span-1">
+                            <TotalSalesCard />
+                        </div>
+                        <div className="sm:cols-4 col-span-1">
+                            <TotalSalesCard />
+                        </div>
+                        <div className="sm:cols-4 col-span-1">
+                            <TotalSalesCard />
+                        </div>
 
+                    </div>
+                    <div className="grid grid-cols-1 h-96 gap-4">
+                        <div className="grid grid-cols-4 gap-4">
+                            <div className="col-span-1">
+                                <SalesChartCard />
+                            </div>
+                            <div className="col-span-2">
+                                <SalesLineChartCard />
+                            </div>
+                            <div className="col-span-1">
+                                <SalesByStoreChart />
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-4 gap-4">
+                            <div className="col-span-2">
+                                <SalesLineChartCard />
+                            </div>
+                            <div className="col-span-1">
+                                <SalesChartCard />
+                            </div>
+                            <div className="col-span-1">
+                                <SalesByStoreChart />
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-4 gap-4">
+                            <div className="col-span-2">
+                                <SalesLineChartCard />
+                            </div>
+                            <div className="col-span-1">
+                                <SalesChartCard />
+                            </div>
+                            <div className="col-span-1">
+                                <SalesByStoreChart />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </main>
-            <footer className="py-0.5 px-4 bg-gray-800 text-neutral-500">
+            <footer className="fixed bottom-0 w-full py-0.5 px-4 bg-neutral-800 text-neutral-500">
                 <label className="text-neutral-400 text-sm">usuário :</label>
                 <span className="text-sm"> {userName}</span>
             </footer>
