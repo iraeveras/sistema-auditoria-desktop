4210b455054f12f9b84c034f9d4b4b47e3c9c20d
Irae Veras <irae.online@gmail.com>
2025-03-22 19:25:26 -0300

    style: alteração do bg do footer

--- src/ui/pages/Dashboard.tsx
+++ src/ui/pages/Dashboard.tsx
@@ -20,7 +20,7 @@ const Dashboard: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen bg-neutral-50 overflow-hidden">
             <header className="fixed top-0 w-full z-10 py-3 p-4 bg-neutral-800 text-white">Dashboard</header>
-            <main className="flex-1 mt-11 p-4 overflow-y-scroll mb-11">
+            <main className="flex-1 mt-11 p-4 overflow-y-auto mb-11">
                 <div className="flex flex-col gap-4">
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
                         <div className="sm:cols-4 col-span-1">
@@ -50,34 +50,16 @@ const Dashboard: React.FC = () => {
                             </div>
                         </div>
 
-                        <div className="grid grid-cols-4 gap-4">
-                            <div className="col-span-2">
-                                <SalesLineChartCard />
-                            </div>
-                            <div className="col-span-1">
-                                <SalesChartCard />
-                            </div>
-                            <div className="col-span-1">
-                                <SalesByStoreChart />
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-4 gap-4">
-                            <div className="col-span-2">
-                                <SalesLineChartCard />
-                            </div>
-                            <div className="col-span-1">
-                                <SalesChartCard />
-                            </div>
-                            <div className="col-span-1">
-                                <SalesByStoreChart />
-                            </div>
-                        </div>
+                        
+                        
                     </div>
                 </div>
             </main>
-            <footer className="fixed bottom-0 w-full py-0.5 px-4 bg-neutral-800 text-neutral-500">
-                <label className="text-neutral-400 text-sm">usuário :</label>
-                <span className="text-sm"> {userName}</span>
+            <footer className="fixed flex items-center bottom-0 w-full py-0.5 px-4 bg-neutral-800 ">
+                <div className="bg-transparent text-neutral-500">
+                    <span className="text-neutral-400 text-sm">usuário:</span>
+                    <span className="text-sm"> {userName}</span>
+                </div>
             </footer>
         </div>
     )
