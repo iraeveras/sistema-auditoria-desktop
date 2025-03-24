1fac495f6534c915976ab2caafdeb4321781f3a1
Irae Veras <irae.online@gmail.com>
2025-03-24 00:34:01 -0300

    feat: adicioado o campo id

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,8 +3,9 @@ import axios from "axios";
 
 const API_BASE_URL = 'https://back-auditoria.onrender.com';
 
-export const signin = async (username: string, password: string) => {
+export const signin = async (id: number, username: string, password: string) => {
     const response = await axios.post(`${API_BASE_URL}/auth/signIn/`, {
+        id,
         username,
         password,
     });
