2dff9572df9b7b00a06a65364d28081178b2de86
Irae Veras <irae.online@gmail.com>
2025-03-25 00:38:04 -0300

    feat: alteração do nome do component da pagina principal pra Main

--- src/ui/pages/Main.tsx
+++ src/ui/pages/Main.tsx
@@ -1,9 +1,10 @@
 // File: src/ui/pages/Dashboard.tsx
 import React, { useEffect, useState } from "react";
-import SalesChartCard from "../components/dashboard/charts/SalesChartCard";
-import SalesLineChartCard from "../components/dashboard/charts/SalesLineChartCard";
-import TotalSalesCard from "../components/dashboard/cards/TotalSalesCard";
-import SalesByStoreChart from "../components/dashboard/charts/SalesByStoreChart";
+import { DashboardPage } from "./DashboardPage";
+// import SalesChartCard from "../components/dashboard/charts/SalesChartCard";
+// import SalesLineChartCard from "../components/dashboard/charts/SalesLineChartCard";
+// import TotalSalesCard from "../components/dashboard/cards/TotalSalesCard";
+// import SalesByStoreChart from "../components/dashboard/charts/SalesByStoreChart";
 
 const Dashboard: React.FC = () => {
     const [userName, setUserName] = useState('');
@@ -21,7 +22,7 @@ const Dashboard: React.FC = () => {
         <div className="flex flex-col min-h-screen bg-neutral-50 overflow-hidden">
             <header className="fixed top-0 w-full z-10 py-3 p-4 bg-neutral-800 text-white">Dashboard</header>
             <main className="flex-1 mt-11 p-4 overflow-y-auto mb-11">
-                <div className="flex flex-col gap-4">
+                {/* <div className="flex flex-col gap-4">
                     <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
                         <div className="sm:cols-4 col-span-1">
                             <TotalSalesCard />
@@ -53,7 +54,8 @@ const Dashboard: React.FC = () => {
                         
                         
                     </div>
-                </div>
+                </div> */}
+                <DashboardPage/>
             </main>
             <footer className="fixed flex items-center bottom-0 w-full py-0.5 px-4 bg-neutral-800 ">
                 <div className="bg-transparent text-neutral-500">
